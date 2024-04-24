b366d4b3c68e37bdb44c4a13d9135712f35b67ea
celestino <celestinofigus@outlook.com>
2024-04-24 14:16:40 +0200

    name button

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
